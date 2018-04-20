31c9fdda2bac35ddb72fa51103c3eecb84fee828
aruyuna <33777205+aruyuna9531@users.noreply.github.com>
2018-04-20 21:34:46 +0800

    Update heartcounter.js

--- heartcounter.js
+++ heartcounter.js
@@ -28,9 +28,7 @@ function EventOpening(date){
 	if(date.getDate()==15 && date.getHours()>=14)return false;
 	if(date.getDate()>15 && date.getDate()<20)return false;
 	if(date.getDate()==20 && date.getHours()<15)return false;
-	if(date.getDate()==LastDayOfCurrentMonth(date.getFullYear
-
-(),date.getMonth()+1) && date.getHours()>=14)return false;
+	if(date.getDate()==LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1) && date.getHours()>=14)return false;
 	return true;
 }
 
@@ -38,26 +36,12 @@ function EventEnd(date){
 	if(date.getDate()==5 && date.getHours()>=15 || 
 	date.getDate()>5 && date.getDate()<20 || 
 	date.getDate()==20 && date.getHours()<15
-	)return new Date(date.getFullYear()+"-"+(date.getMonth()+1)+"-15 
-
-14:00:00");
+	)return new Date(date.getFullYear()+"-"+(date.getMonth()+1)+"-15 14:00:00");
 	if(date.getDate()==20 && date.getHours()>=15 ||
-	date.getDate()>20 && date.getDate()<LastDayOfCurrentMonth
-
-(date.getFullYear(),date.getMonth()+1) || 
-	date.getDate()==LastDayOfCurrentMonth(date.getFullYear(),date.getMonth
-
-()+1)
-	)return new Date(date.getFullYear()+"-"+(date.getMonth
-
-()+1)+"-"+LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1)+" 
-
-14:00:00");
-	if(date.getMonth()!=0)return new Date(date.getFullYear
-
-()+"-"+(date.getMonth())+"-"+LastDayOfCurrentMonth(date.getFullYear
-
-(),date.getMonth())+" 14:00:00");
+	date.getDate()>20 && date.getDate()<LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1) || 
+	date.getDate()==LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1)
+	)return new Date(date.getFullYear()+"-"+(date.getMonth()+1)+"-"+LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1)+" 14:00:00");
+	if(date.getMonth()!=0)return new Date(date.getFullYear()+"-"+(date.getMonth())+"-"+LastDayOfCurrentMonth(date.getFullYear(),date.getMonth())+" 14:00:00");
 	else return new Date((date.getFullYear()-1)+"-12-31 14:00:00");
 }
 
@@ -102,46 +86,30 @@ function warn(obj){
 }
 
 function AutomaticRecover(){
-	var RemainMins = Number(document.getElementById("_d").innerHTML)
+	var RemainMins = Number(document.getElementById("_d").innerHTML)*24*60+Number(document.getElementById("_h").innerHTML)+Number(document.getElementById
 
-*24*60+Number(document.getElementById("_h").innerHTML)+Number
-
-(document.getElementById("_m").innerHTML);
+("_m").innerHTML);
 	var AutoRec=Math.floor((RemainMins>10?RemainMins-10:0)/6);
-	var LvUpRec=Number(document.getElementById("LvUp").value) * Number
-
-(document.getElementById("LPMax").value) +1;
+	var LvUpRec=Number(document.getElementById("LvUp").value) * Number(document.getElementById("LPMax").value) +1;
 	var LP50Rec=Number(document.getElementById("LP50").value)*50;
-	var LP50pcRec=Number(document.getElementById("LP50pc").value)*Number
-
-(document.getElementById("LPMax").value)*0.5;
-	var LP100pcRec=Number(document.getElementById("LP100pc").value)*Number
-
-(document.getElementById("LPMax").value);
-	document.getElementById("LPs").innerHTML=AutoRec+LvUpRec+LP50Rec
-
-+LP50pcRec+LP100pcRec;
+	var LP50pcRec=Number(document.getElementById("LP50pc").value)*Number(document.getElementById("LPMax").value)*0.5;
+	var LP100pcRec=Number(document.getElementById("LP100pc").value)*Number(document.getElementById("LPMax").value);
+	document.getElementById("LPs").innerHTML=AutoRec+LvUpRec+LP50Rec+LP50pcRec+LP100pcRec;
 	countres();
 }
 
 function needlps(){
 	var i = 0;
 	var icon = Number(document.getElementById("IconGet").value);
-	var ptcha = Number(document.getElementById("aim").value)-Number
-
-(document.getElementById("cur").value);
+	var ptcha = Number(document.getElementById("aim").value)-Number(document.getElementById("cur").value);
 	var curic = Number(document.getElementById("curicons").value);
 	var ptneeds = ptcha > 0 ? ptcha : 0;
 	while(true){
-		if(icon*i+EventSongPt()*Math.floor((icon*i+curic)/75)
-
->=ptneeds)break;
+		if(icon*i+EventSongPt()*Math.floor((icon*i+curic)/75)>=ptneeds)break;
 		i++;
 	}
 	document.getElementById("normalsong").innerHTML = i;
-	document.getElementById("NeedLP").innerHTML = i*Number
-
-(document.getElementById("LPUse").value);
+	document.getElementById("NeedLP").innerHTML = i*Number(document.getElementById("LPUse").value);
 	countres();
 }
 
@@ -149,9 +117,7 @@ function countres(){
 	var needlp = Number(document.getElementById("NeedLP").innerHTML);
 	var recovlp= Number(document.getElementById("LPs").innerHTML);
 	var lpmax  = Number(document.getElementById("LPMax").value);
-	document.getElementById("auto").innerHTML=Math.ceil((needlp-
-
-recovlp)/lpmax);
+	document.getElementById("auto").innerHTML=Math.ceil((needlp-recovlp)/lpmax);
 }
 
 function EventSongPt(){
