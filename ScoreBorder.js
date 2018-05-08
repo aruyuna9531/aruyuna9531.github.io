4e113f0ef95f352774734e236beef70f44c89001
aruyuna9531 <982033268@qq.com>
2018-05-08 22:38:46 +0800

    modified:   ScoreBorder.html 	modified:   ScoreBorder.js

--- ScoreBorder.js
+++ ScoreBorder.js
@@ -23,25 +23,96 @@ function print(){
 	var S1 = Number(document.getElementById("FirstBase").innerHTML);
 	var S2 = Number(document.getElementById("SecondBase").innerHTML);
 	var S3 = Number(document.getElementById("ThirdBase").innerHTML);
-<<<<<<< HEAD
-	var T1 = Number(document.getElementById("T1").innerHTML);
-	var T2 = Number(document.getElementById("T2").innerHTML);
-	var T3 = Number(document.getElementById("T3").innerHTML);
+	var TeaTimeSpot = Number(document.getElementById("_d").innerHTML)*24+Number(document.getElementById("_h").innerHTML);
 	document.getElementById("First").innerHTML=Math.round(arrangeBits(EType)*comboBonus(maxC)*S1);
 	document.getElementById("Second").innerHTML=Math.round(arrangeBits(EType)*comboBonus(maxC)*S2);
 	document.getElementById("Third").innerHTML=Math.round(arrangeBits(EType)*comboBonus(maxC)*S3);
-	document.getElementById("Tea1").innerHTML=Math.round(arrangeBits(EType)*comboBonus(maxC)*T1);
-	document.getElementById("Tea2").innerHTML=Math.round(arrangeBits(EType)*comboBonus(maxC)*T2);
-	document.getElementById("Tea3").innerHTML=Math.round(arrangeBits(EType)*comboBonus(maxC)*T3);
+	document.getElementById("Tea1").innerHTML=Math.round(arrangeBits(EType)*comboBonus(maxC)*(S1+TeaTimeSpot/8));
+	document.getElementById("Tea2").innerHTML=Math.round(arrangeBits(EType)*comboBonus(maxC)*(S2+TeaTimeSpot/9));
+	document.getElementById("Tea3").innerHTML=Math.round(arrangeBits(EType)*comboBonus(maxC)*(S3+TeaTimeSpot/10));
 }
-=======
-	var are = arrangeBits(EType);
-	var cbs = comboBonus(maxC);
-	document.getElementById("First").innerHTML=Math.round(are*cbs*S1);
-	document.getElementById("Second").innerHTML=Math.round(are*cbs*S2);
-	document.getElementById("Third").innerHTML=Math.round(are*cbs*S3);
-	document.getElementById("Tea1").innerHTML=Math.round(are*cbs*(S1+20));
-	document.getElementById("Tea2").innerHTML=Math.round(are*cbs*(S2+18));
-	document.getElementById("Tea3").innerHTML=Math.round(are*cbs*(S3+16));
+
+function isNull(obj){
+        if (obj == '' || obj == undefined || obj == null) {
+		obj.value=0;
+	}
 }
->>>>>>> 5a9a8a1ec8afb750fd5a36dd82e6bd4c6861cfbd
+
+function LastDayOfCurrentMonth(year,month){
+	switch(month){
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:return 31;
+	case 4:
+	case 6:
+	case 9:
+	case 11:return 30;
+	default:if(year%4==0)return 29;else return 28;
+	}
+}
+
+function EventOpening(date){
+	if(date.getDate()<5)return false;
+	if(date.getDate()==5 && date.getHours()<15)return false;
+	if(date.getDate()==15 && date.getHours()>=14)return false;
+	if(date.getDate()>15 && date.getDate()<20)return false;
+	if(date.getDate()==20 && date.getHours()<15)return false;
+	if(date.getDate()==LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1) && date.getHours()>=14)return false;
+	return true;
+}
+
+function EventEnd(date){
+	if(date.getDate()==5 && date.getHours()>=15 || 
+	date.getDate()>5 && date.getDate()<20 || 
+	date.getDate()==20 && date.getHours()<15
+	)return new Date(date.getFullYear()+"-"+(date.getMonth()+1)+"-15 14:00:00");
+	if(date.getDate()==20 && date.getHours()>=15 ||
+	date.getDate()>20 && date.getDate()<LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1) || 
+	date.getDate()==LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1)
+	)return new Date(date.getFullYear()+"-"+(date.getMonth()+1)+"-"+LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1)+" 14:00:00");
+	if(date.getMonth()!=0)return new Date(date.getFullYear()+"-"+(date.getMonth())+"-"+LastDayOfCurrentMonth(date.getFullYear(),date.getMonth())+" 14:00:00");
+	else return new Date((date.getFullYear()-1)+"-12-31 14:00:00");
+}
+
+function countTime() {
+        var date = new Date(new Date().getTime());
+        var now = date.getTime();  
+        var endDate = EventEnd(date);
+        var end = endDate.getTime();
+        var leftTime = end-now; 
+        var d=0,h=0,m=0,s=0;  
+        if (leftTime>0 && EventOpening(date)==true) {
+	d = Math.floor(leftTime/1000/60/60/24);  
+        h = Math.floor(leftTime/1000/60/60%24);  
+        m = Math.floor(leftTime/1000/60%60);  
+        s = Math.floor(leftTime/1000%60);                     
+        }
+        document.getElementById("_d").innerHTML = d;
+	document.getElementById("_h").innerHTML = h;
+	document.getElementById("_m").innerHTML = front0(m);
+	document.getElementById("_s").innerHTML = front0(s);
+	if(d<1){
+		warn(_d);
+		if(h<10)
+		{
+			warn(_h);
+			if(h<2){
+				warn(_m);
+				warn(_s);
+			}
+		}
+	}
+        setTimeout(countTime,1000);
+}
+function front0(x){
+	if(x<10)return "0"+x;
+	else return x;
+}
+function warn(obj){
+	obj.style.color="red";
+	obj.style.fontWeight="Bold";
+}
