cde2feebd4b078f0e3a2a7e252c4ccc3c3fef74b
aruyuna9531 <982033268@qq.com>
2018-06-15 13:13:42 +0800

    new file:   SafetyTest.html 	new file:   SafetyTest.js

---
+++ SafetyTest.js
@@ -0,0 +1,110 @@
+function isNull(obj){
+        if (obj == '' || obj == undefined || obj == null) {
+		obj.value=0;
+	}
+}
+
+function LastDayOfCurrentMonth(year,month){
+	switch(month){
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:return 31;
+	case 4:
+	case 6:
+	case 9:
+	case 11:return 30;
+	default:if(year%4==0)return 29;else return 28;
+	}
+}
+
+function EventOpening(date){
+	if(date.getDate()<5)return false;
+	if(date.getDate()==5 && date.getHours()<15)return false;
+	if(date.getDate()==15 && date.getHours()>=14)return false;
+	if(date.getDate()>15 && date.getDate()<20)return false;
+	if(date.getDate()==20 && date.getHours()<15)return false;
+	if(date.getDate()==LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1) && date.getHours()>=14)return false;
+	return true;
+}
+
+function EventEnd(date){
+	if(date.getDate()==5 && date.getHours()>=15 || 
+	date.getDate()>5 && date.getDate()<20 || 
+	date.getDate()==20 && date.getHours()<15
+	)return new Date(date.getFullYear()+"/"+(date.getMonth()+1)+"/15 14:00:00");
+	if(date.getDate()==20 && date.getHours()>=15 ||
+	date.getDate()>20 && date.getDate()<LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1) || 
+	date.getDate()==LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1)
+	)return new Date(date.getFullYear()+"/"+(date.getMonth()+1)+"/"+LastDayOfCurrentMonth(date.getFullYear(),date.getMonth()+1)+" 14:00:00");
+	if(date.getMonth()!=0)return new Date(date.getFullYear()+"/"+(date.getMonth())+"/"+LastDayOfCurrentMonth(date.getFullYear(),date.getMonth())+" 14:00:00");
+	else return new Date((date.getFullYear()-1)+"/12/31 14:00:00");
+}
+
+function countTime() {
+        var date;
+	var u = navigator.userAgent;
+	var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+	if(isiOS)date = new Date();
+	else date = new Date();
+        var now = date.getTime();  
+        var endDate = EventEnd(date);
+        var end = endDate.getTime();
+        var leftTime = end-now; 
+        var d=0,h=0,m=0,s=0;
+	var preMin=Number(document.getElementById("_m").innerHTML);
+        if (leftTime>0 && EventOpening(date)==true) {
+	d = Math.floor(leftTime/1000/60/60/24);  
+        h = Math.floor(leftTime/1000/60/60%24);  
+        m = Math.floor(leftTime/1000/60%60);  
+        s = Math.floor(leftTime/1000%60);                     
+        }
+        document.getElementById("_d").innerHTML = d;
+	document.getElementById("_h").innerHTML = h;
+	document.getElementById("_m").innerHTML = front0(m);
+	document.getElementById("_s").innerHTML = front0(s);
+	if(d<1){
+		warn(_d);
+		if(h<10)
+		{
+			warn(_h);
+			if(h<2){
+				warn(_m);
+				warn(_s);
+			}
+		}
+	}
+	if(preMin!=m)fix();
+        setTimeout(countTime,1000);
+}
+function front0(x){
+	if(x<10)return "0"+x;
+	else return x;
+}
+function warn(obj){
+	obj.style.color="red";
+	obj.style.fontWeight="Bold";
+}
+//---------------------------CountTimeLine------------------------------
+
+function change(){
+	var lastRank=Number(document.getElementById("border").value);
+	var curRank=Number(document.getElementById("curRank").value);
+	var remainMin=Number(document.getElementById("_d").innerHTML)*24*60+Number(document.getElementById("_h").innerHTML)*60+Number(document.getElementById("_m").innerHTML);
+	var drop=Math.floor((lastRank-curRank)/remainMin);
+	document.getElementById("nextRank").innerHTML=curRank+drop-1;
+	if(remainMin>5)document.getElementById("nextRank_5").innerHTML=curRank+drop*5-10;
+	else document.getElementById("nextRank_5").innerHTML=""
+	if(remainMin>10)document.getElementById("nextRank_10").innerHTML=curRank+drop*10-20;
+	else document.getElementById("nextRank_10").innerHTML=""
+	if(remainMin>30)document.getElementById("nextRank_30").innerHTML=curRank+drop*30-50;
+	else document.getElementById("nextRank_30").innerHTML=""
+}
+
+function fix(){
+	document.getElementById("curRank").value=Number(document.getElementById("nextRank").innerHTML);
+	change();
+}
