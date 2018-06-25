a9197f4a9c49b205948e4e9616a0478cb3ff369e
aruyuna <33777205+aruyuna9531@users.noreply.github.com>
2018-06-25 13:41:55 +0800

    Update SafetyTest.js

--- SafetyTest.js
+++ SafetyTest.js
@@ -107,8 +107,14 @@ function change(){
 	else document.getElementById("nextRank_10").innerHTML=""
 	if(remainMin>30)document.getElementById("nextRank_30").innerHTML=curRank+drop*30-50;
 	else document.getElementById("nextRank_30").innerHTML=""
-	if(drop<=14)document.getElementById("msgC").innerHTML=remainMin>15?"掉档风险极大，建议立即打歌，5首":"掉档风险极大，赶紧打歌，能打几首打几首";
-	else if(drop<=20)document.getElementById("msgC").innerHTML="掉档风险较大，建议立即打1首歌";
+	var HighRisk=0, LittleRisk=1;
+	switch(lastRank){
+		case 10000:HighRisk=18;LittleRisk=25;break;
+		case 30000:HighRisk=25;LittleRisk=40;break;
+		case 50000:HighRisk=50;LittleRisk=75;break;
+	}
+	if(drop<=HighRisk)document.getElementById("msgC").innerHTML=remainMin>15?"掉档风险极大，建议立即打歌，5首":"掉档风险极大，赶紧打歌，能打几首打几首";
+	else if(drop<=LittleRisk)document.getElementById("msgC").innerHTML="掉档风险较大，建议立即打1首歌";
 	else document.getElementById("msg").innerHTML="仅供参考，如果1分钟后你的排名在上面安全排名之后，建议打歌保档";
 }
 
@@ -184,8 +190,14 @@ function changeC(){
 	else document.getElementById("nextRank_10C").innerHTML=""
 	if(remainMin>30)document.getElementById("nextRank_30C").innerHTML=curRank+drop*30-50;
 	else document.getElementById("nextRank_30C").innerHTML=""
-	if(drop<=4)document.getElementById("msgC").innerHTML="掉档风险极大，建议立即打歌，5首";
-	else if(drop<=7)document.getElementById("msgC").innerHTML="掉档风险较大，建议立即打1首歌";
+	var HighRisk=0, LittleRisk=1;
+	switch(lastRank){
+		case 2300:HighRisk=5;LittleRisk=8;break;
+		case 6900:HighRisk=8;LittleRisk=11;break;
+		case 11500:HighRisk=12;LittleRisk=16;break;
+	}
+	if(drop<=HighRisk)document.getElementById("msgC").innerHTML=remainMin>15?"掉档风险极大，建议立即打歌，5首":"掉档风险极大，赶紧打歌，能打几首打几首";
+	else if(drop<=LittleRisk)document.getElementById("msgC").innerHTML="掉档风险较大，建议立即打1首歌";
 	else document.getElementById("msgC").innerHTML="仅供参考，如果1分钟后你的排名在上面安全排名之后，建议打歌保档";
 }
 
