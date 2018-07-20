5d76441b21f6d0c9c358fd31f32dcd23bfb75a1d
yuna <33777205+aruyuna9531@users.noreply.github.com>
2018-07-20 11:16:58 +0800

    Update LoveLiveSIFLovecaCalculator.js

--- LoveLiveSIFLovecaCalculator.js
+++ LoveLiveSIFLovecaCalculator.js
@@ -278,7 +278,8 @@ function AqoursBirthGacha(strategy){
 function MuseMemberGachaMax(){
 	var this80=0;
 	if(document.getElementById("this80").value=="yes")this80=1;
-	var Loveca = Number(document.getElementById("totalloveca").innerHTML)-80*this80-Number(document.getElementById("NumazuKanshasai").value);
+	var tmp=Number(document.getElementById("NumazuKanshasai").value);
+	var Loveca = Number(document.getElementById("totalloveca").innerHTML)-80*this80-(tmp==null?0:tmp);
 	var Ticket = Number(document.getElementById("totalTicket").innerHTML);
 	var Chain_11 = Math.floor(Loveca/50);
 	var single = Math.floor((Loveca-Chain_11*50)/5);
