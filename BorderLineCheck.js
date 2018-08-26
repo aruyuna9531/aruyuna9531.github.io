fbb3027162d1812243767b62b04531c6dbe5ff0d
yuna <33777205+aruyuna9531@users.noreply.github.com>
2018-08-26 11:46:10 +0800

    Update BorderLineCheck.js

--- BorderLineCheck.js
+++ BorderLineCheck.js
@@ -26,7 +26,7 @@ function detect(curEv,b){
 	var difID=curEv+"dif";
 	var dif=Number(document.getElementById(difID).innerHTML);
 	
-	document.getElementById(cID).innerHTML=Math.round(eScore*bc*ar*dif/80);
+	document.getElementById(cID).innerHTML=Math.round(eScore*bc*ar*dif/80*1.21);
 	warn2(cID);
 }
 function warn(obj){
@@ -78,7 +78,7 @@ function e(curEv,b){
 	var cID=curEv+String(b)+"C";
 	var difID=curEv+"dif";
 	var dif=Number(document.getElementById(difID).innerHTML);
-	document.getElementById(cID).innerHTML=Math.round(eScore/bc/ar/dif*80);
+	document.getElementById(cID).innerHTML=Math.round(eScore/bc/ar/dif*80/1.21);
 	warn(cID);
 }
 //-------------------------%--------------------------------
